f0f12c9cad25321e4aee995190579715eba311cb
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-10 15:24:17 -0600

    Resizing and background image NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
-
+import imgIce from '../Assets/imgs/fabrizio-conti-aExT3y92x5o-unsplash.jpg';
 
 const Nav = styled.nav`
-
     /* GENERAL */
-
+    background-image: url(${imgIce});
     box-shadow: 0rem 0.5rem 2rem 2.5rem rgba(54, 168, 239, 1),
     0rem 0.5rem 1rem 0.5rem rgba(54, 168, 239, 1) inset;
     backdrop-filter: blur(1rem);
@@ -20,17 +19,23 @@ const Nav = styled.nav`
     align-items: center;
     
 
-    /* FONTS */
+    /* NESTED */
     
     a {
-        font-family: 'Iceberg', cursive;
-        font-size: 2.5rem;
-        font-weight: 400;
-        text-decoration: none; 
-        text-align: center;
+        /* GENERAL */
+
         background: linear-gradient(0deg, #5AD3FF, #50C2F4, #46B1E9, #3CA1DE, #3290D3, #287FC8, #1E6EBD, #145EB2, #0A4DA7, #003C9C);
         background-clip: text;
         -webkit-text-fill-color: transparent;
+
+        /* FONT */
+
+        font-family: 'Iceberg', cursive;
+        font-size: 2rem;
+        font-weight: 400;
+        text-decoration: none; 
+        text-align: center;
+        
 
         &:hover {
             text-shadow: 0rem 0.1rem 0rem rgba(255,255,255,0.8);

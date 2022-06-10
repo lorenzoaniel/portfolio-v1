79fad13bebfcd37b8bbc007b64e3a4b1e9a3946f
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-09 23:10:21 -0600

    Basic NavBar look NavBar.js
    added font family and fixed layout as well as adding gradients to menu titles. basic hover animation.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,15 +2,46 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
+
 const Nav = styled.nav`
+
+    /* GENERAL */
+
+    box-shadow: 0rem 0.5rem 2rem 2.5rem rgba(54, 168, 239, 1),
+    0rem 0.5rem 1rem 0.5rem rgba(54, 168, 239, 1) inset;
+    backdrop-filter: blur(1rem);
+    border-bottom: 0.5rem groove rgba(255, 255, 255, 0.1);
+
+    /* GRID/FLEX */
+
     grid-area: ${props => props.gridArea};
-    background: blue;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    
+
+    /* FONTS */
+    
+    a {
+        font-family: 'Iceberg', cursive;
+        font-size: 2.5rem;
+        font-weight: 400;
+        text-decoration: none; 
+        text-align: center;
+        background: linear-gradient(0deg, #5AD3FF, #50C2F4, #46B1E9, #3CA1DE, #3290D3, #287FC8, #1E6EBD, #145EB2, #0A4DA7, #003C9C);
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+
+        &:hover {
+            text-shadow: 0rem 0.1rem 0rem rgba(255,255,255,0.8);
+        }
+    }
 `;
 
 const NavBar = (props) => {
     return (
         <Nav {...props}>
-            <Link to={"/"}><></>About</Link>
+            <Link to={"/"}>About</Link>
             <Link to={"/projects"}>Projects</Link>
             <Link to={"/contact"}>Contact</Link>
         </Nav>

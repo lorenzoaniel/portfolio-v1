d8827e01f435d66af2eac2d52a0eff3df469b011
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-10 19:15:19 -0600

    Update NavBar.jsx
    added floating ice menu things

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,26 +2,83 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import imgIce from '../Assets/imgs/fabrizio-conti-aExT3y92x5o-unsplash.jpg';
+import getRandomValue from '../Helpers/getRandomVal';
 
 const Nav = styled.nav`
     /* GENERAL */
-    background-image: url(${imgIce});
-    /* box-shadow: 0rem 0.5rem 2rem 2.5rem rgba(54, 168, 239, 1),
-    0rem 0.5rem 1rem 0.5rem rgba(54, 168, 239, 1) inset; */
-    backdrop-filter: blur(1rem);
 
     /* GRID/FLEX */
     grid-area: ${props => props.gridArea};
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    
+
+    @keyframes scaleMenuItems{
+        0%{
+            transform: translate3d(0, 0, 0);
+        }
+        25%{
+            transform: translate3d(0, 0, 0);
+        }
+        50%{
+            transform: translate3d(0, 0, 0);
+        }
+        75%{
+            transform: translate3d(0, 0, 0);
+        }
+        100%{
+            transform: translate3d(0, 0, 0);
+        }
+    }
+`;
+
+const Div = styled.div`
+    /* GENERAL */
+    height: 100%;
+    background-image: url(${imgIce});
+    background-size: contain;
+    clip-path: polygon(35% 0, 92% 3%, 100% 35%, 93% 65%, 80% 90%, 53% 82%, 35% 93%, 5% 80%, 0% 35%, 8% 16%);
+    border-radius: 10px;
+
+    /* GRID/FLEX */
+    flex-basis: 33%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     /* ANIMATION */
-    animation: navBarFloat 3s linear infinite;
+    animation: elementFloat 2s linear infinite;
+    animation-delay: ${props => props.delayVal}s;
     animation-direction: alternate;
 
+    /* NESTED */
+        a {
+            /* GENERAL */
+            background: linear-gradient(0deg, #5AD3FF, #50C2F4, #46B1E9, #3CA1DE, #3290D3, #287FC8, #1E6EBD, #145EB2, #0A4DA7, #003C9C);
+            background-clip: text;
+            -webkit-text-fill-color: transparent;
+            text-shadow: 0 0.1rem 0.1rem rgba(0,0,0,0.5);
+
+            /* FONT */
+            font-family: 'Iceberg', cursive;
+            font-size: 3.5rem;
+            font-weight: 400;
+            text-decoration: none; 
+            text-align: center;
+            
+            /* ANIMATION */
+            animation: scaleMenuItems 3s linear infinite;
+            animation-direction: alternate;
+
+
+            &:hover {
+                text-shadow: 0rem 0.1rem 0rem rgba(255,255,255,0.5);
+            }
+        }
+    }
+    
     /* KEYFRAMES */ 
-    @keyframes navBarFloat{
+    @keyframes elementFloat{
         0%{
             transform: translate3d(0, 0, 0);
             box-shadow: 0rem 1rem 1.5rem 0.5rem rgba(54, 168, 239, 1),
@@ -52,38 +109,15 @@ const Nav = styled.nav`
             0rem -0.2rem 1.5rem 0.4rem rgba(54, 168, 239, 1),
             0rem -0.5rem 1.5rem 0.5rem rgba(54, 168, 239, 1) inset;
         }
-    }
-
-    /* NESTED */
     
-    a {
-        /* GENERAL */
-
-        background: linear-gradient(0deg, #5AD3FF, #50C2F4, #46B1E9, #3CA1DE, #3290D3, #287FC8, #1E6EBD, #145EB2, #0A4DA7, #003C9C);
-        background-clip: text;
-        -webkit-text-fill-color: transparent;
-
-        /* FONT */
-
-        font-family: 'Iceberg', cursive;
-        font-size: 2rem;
-        font-weight: 400;
-        text-decoration: none; 
-        text-align: center;
-        
-
-        &:hover {
-            text-shadow: 0rem 0.1rem 0rem rgba(255,255,255,0.5);
-        }
-    }
 `;
 
 const NavBar = (props) => {
     return (
         <Nav {...props}>
-            <Link to={"/"}>About</Link>
-            <Link to={"/projects"}>Projects</Link>
-            <Link to={"/contact"}>Contact</Link>
+            <Div delayVal={getRandomValue(1,2)}><Link to={"/"}><span>About</span></Link></Div>
+            <Div delayVal={getRandomValue(2,3)}><Link to={"/projects"}>Projects</Link></Div>
+            <Div delayVal={getRandomValue(3,4)}><Link to={"/contact"}>Contact</Link></Div>
         </Nav>
     )
 }

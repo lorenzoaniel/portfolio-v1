03735085d19628b1d1300a366db510ba0c144354
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-14 17:27:09 -0600

    made iceberg global
    made iceberg global

--- src/Mixins/GlobalStyle.js
+++ src/Mixins/GlobalStyle.js
@@ -4,7 +4,15 @@ const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        box-sizing: border-box; 
+        box-sizing: border-box;
+        border: none;
+
+        /* FONT */
+        font-family: 'Iceberg', cursive;
+        font-weight: 400;
+        text-decoration: none; 
+        letter-spacing: 0.1em;
+        text-shadow: 0 0.1rem 0.1rem rgba(0,0,0,0.5);
     }
 
     html {

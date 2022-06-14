cf3ccc4567e45af6d078c1f16279fdefbb78222b
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-14 17:26:47 -0600

    Update About.jsx
    cosmetics

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import AboutIntroduction from '../Components/AboutIntroduction';
+import DontPressWarning from '../Components/DontPressWarning';
+
+
+
+const AboutPageConfig = {
+  aboutIntro: {
+    gridArea: "1",
+  },
+  dontPress: {
+    gridArea: "2",
+  }
+}
 
 const Div = styled.div`
-  display: flex;
+  display: grid;
   justify-content: center;
   align-items: center;
+  padding: 1.5rem;
   grid-area: ${props => props.gridArea};
+  grid-template-columns: 1fr;
+  grid-template-rows: 60% 40%;
+  grid-template-areas: 
+  "1"
+  "2"
+  ;
 `;
 
 const About = (props) => {
+  
   return (
     <Div {...props}>
-      <h1>About</h1>
+      <AboutIntroduction gridArea={AboutPageConfig.aboutIntro.gridArea}/>
+      <DontPressWarning gridArea={AboutPageConfig.dontPress.gridArea}/>
     </Div>
   );
 }

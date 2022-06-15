59b8473ef391d25ca873d9a5812d1441559f4842
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-15 16:30:55 -0600

    Create PressMeTaunts.js
    button quote taunts

---
+++ src/Local-Data/TauntingButtonData/PressMeTaunts.js
@@ -0,0 +1,11 @@
+const pressMeTaunts = {
+    taunts: [
+        "You have nothing better to do why not...",
+        "They lie to you...",
+        "I have cookies...",
+        "Untold riches, if you but summon me...",
+        "Fear is for fools, so fear not--summon me!",
+    ]
+}
+
+export default pressMeTaunts;

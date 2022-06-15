7423a39e3cc889e263bebb7bfadc100d436d98ca
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-15 16:31:08 -0600

    Create DontPressWarningQuotes.js
    quotes for warning sign

---
+++ src/Local-Data/DontPressData/DontPressWarningQuotes.js
@@ -0,0 +1,16 @@
+const dontPressWarning = {
+    warning: [
+        "It Lies...",
+        "Don't Press No Matter What!",
+        "Please Don't Press",
+        "Resist...",
+        "Do Not Heed It's Words",
+        "Mind It Not",
+        "Ignore It",
+        "Heed Our Warning",
+        "We Are The Truth",
+        "We Do Not Lie",
+    ]
+}
+
+export default dontPressWarning;

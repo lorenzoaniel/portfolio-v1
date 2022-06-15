9f4df5225417028c0226f377853df94e66990a30
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-15 16:38:31 -0600

    Create AboutIntroduction.jsx
    introduction container that is on the top half of about page.

---
+++ src/Components/AboutIntroduction.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 0.5rem solid black;
+`;
+
+const AboutIntroduction = (props) => {
+  return (
+    <Div {...props}>
+          Hi! my name is Lorenzo. I am an IT enthusiast and currently interested in a junior front-end development role.
+          Feel free to check out my website and some projects I showcase!
+          Oh, and if possible please ignore the glitchy magic looking part below, I am still new to React and some things are a little finnicky.
+          Have fun exploring! 
+    </Div>
+  );
+}
+
+export default AboutIntroduction;

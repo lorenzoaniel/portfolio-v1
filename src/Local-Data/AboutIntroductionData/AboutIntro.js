cfc400b3aff4095e2016f8735e16e3d18342b8d3
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-21 16:44:21 -0600

    Create AboutIntro.js

---
+++ src/Local-Data/AboutIntroductionData/AboutIntro.js
@@ -0,0 +1,8 @@
+const aboutIntro = {
+    intro: `Hi! my name is Lorenzo. I am an IT enthusiast and currently interested in a junior front-end development role.
+    Feel free to check out my website and some projects I showcase!
+    Oh, and please ignore the glitchy magic looking part below, I am still new to React and Framer, so some things are a little finnicky.
+    Have fun exploring!`,
+}
+
+export default aboutIntro;

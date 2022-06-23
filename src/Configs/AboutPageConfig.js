1cdff2f53954c38d9c436aa004ccce56a0a0ff65
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-22 21:30:09 -0600

    Create AboutPageConfig.js
    originally located with warning sign but moved on its own since it is also utilized for routing on App.jsx

---
+++ src/Configs/AboutPageConfig.js
@@ -0,0 +1,12 @@
+const AboutPageConfig = () => {
+    return {
+        aboutIntro: {
+            gridArea: "1",
+        },
+        dontPress: {
+            gridArea: "2",
+        }     
+    }
+}
+
+export default AboutPageConfig

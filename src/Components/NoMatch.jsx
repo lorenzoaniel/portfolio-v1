5b444c675c41c32ec4064005b4928aa237f6c1f0
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-22 21:30:26 -0600

    Create NoMatch.jsx
    no match for non existing page urls

---
+++ src/Components/NoMatch.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    
+`;
+
+
+const NoMatch = (props) => {
+
+    return(
+        <Div>NO MATCH</Div>
+    )
+};
+
+export default NoMatch;

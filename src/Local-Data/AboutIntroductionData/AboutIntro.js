2f9909372ab892ee483b7a16d3539354c790362a
lorenzoaniel <lorenzoaniel@gmail.com>
2022-06-27 17:53:54 -0600

    Update AboutIntro.js
    minor change

--- src/Local-Data/AboutIntroductionData/AboutIntro.js
+++ src/Local-Data/AboutIntroductionData/AboutIntro.js
@@ -1,5 +1,5 @@
 const aboutIntro = {
-    intro: `Hi! my name is Lorenzo. I am an IT enthusiast and currently interested in a junior front-end development role.
+    intro: `Hi! my name is Lorenzo. I like to learn things that are interesting.
     Feel free to check out my website and some projects I showcase!
     Oh, and please ignore the glitchy magic looking part below, I am still new to React and Framer, so some things are a little finnicky.
     Have fun exploring!`,

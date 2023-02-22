aae6a477d0de71f74f9e0ec1cfca45e20fbf61b5
lorenzoaniel <lorenzoaniel@gmail.com>
2023-02-21 18:47:00 -0700

    Update useCurrentHeight.js

--- src/Helpers/useCurrentHeight.js
+++ src/Helpers/useCurrentHeight.js
@@ -1,35 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const getHeight = () => window.innerHeight
-  || document.documentElement.clientHeight 
-  || document.body.clientHeight;
+const getHeight = () =>
+	window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-function useCurrentHeight() {
-  // save current window Height in the state object
-  let [height, setHeight] = useState(getHeight());
+const getWidth = () =>
+	window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-  // in this case useEffect will execute only once because
-  // it does not have any dependencies.
-  useEffect(() => {
-    // timeoutId for debounce mechanism
-    let timeoutId = null;
-    const resizeListener = () => {
-      // prevent execution of previous setTimeout
-      clearTimeout(timeoutId);
-      // change Height from the state object after 150 milliseconds
-      timeoutId = setTimeout(() => setHeight(getHeight()), 150);
-    };
-    // set resize listener
-    window.addEventListener('resize', resizeListener);
+function useCurrentDimension() {
+	// save current window Height in the state object
+	let [dimension, setDimension] = useState({ height: getHeight(), width: getWidth() });
 
-    // clean up function
-    return () => {
-      // remove resize listener
-      window.removeEventListener('resize', resizeListener);
-    }
-  }, [])
+	// in this case useEffect will execute only once because
+	// it does not have any dependencies.
+	useEffect(() => {
+		// timeoutId for debounce mechanism
+		let timeoutId = null;
+		const resizeListener = () => {
+			// prevent execution of previous setTimeout
+			clearTimeout(timeoutId);
+			// change dimension from the state object after 150 milliseconds
+			timeoutId = setTimeout(() => setDimension({ height: getHeight(), width: getWidth() }), 150);
+		};
+		// set resize listener
+		window.addEventListener("resize", resizeListener);
 
-  return height;
+		// clean up function
+		return () => {
+			// remove resize listener
+			window.removeEventListener("resize", resizeListener);
+		};
+	}, []);
+
+	return dimension;
 }
 
-export default useCurrentHeight;
+export default useCurrentDimension;
